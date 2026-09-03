4de607ae7fd341c51907cf1d6a399b81298e9ffc
Glenn Seljeseth <selje82@hotmail.com>
2026-09-03 21:01:06 +0200

    Added some comments to repetitions.

---
+++ fundamentals/src/bin/02_variables.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let name = "Glenn";
+    let int1 = 2;
+    let char1 = 'C';
+    let double1 = 2.34;
+    let bool1 = true;
+
+    println!("Name: {name}, int: {int1}, char1: {char1}, double1: {double1}, bool1: {bool1}");
+}
--- fundamentals/src/bin/06_repetition.rs
+++ fundamentals/src/bin/06_repetition.rs
@@ -1,23 +1,33 @@
 fn main() {
+    // Setting num1 as int with value of 0.
     let mut num1 = 0;
 
+    // loop until num1 is eq to 5. 
     loop {
         if num1 == 5 {
             break;
         } 
+        // Displays the number after each iteration.
         println!("{num1:?}");
         num1 += 1;
     }
+
+    // Checking that variable num1 is updated after the loop.
     println!();
     println!("New value of num1: {num1}");
     println!();
 
+    // Setting num2 as int with value of 0.
     let mut num2 = 0;
+
+    // While loop when num2 is not equal to 5. 
     while num2 != 5 {
+        // Printing num2 value after each iteration.
         println!("{num2}");
         num2 += 1;
     }
 
+    // Checking that variable num2 is updated after the while loop. 
     println!();
     println!("New value of num2: {num2}");
     println!();
