f574c7cfd5329437e3465dee135523ff6542ca0d
Glenn Seljeseth <selje82@hotmail.com>
2026-09-04 22:17:32 +0200

    Added a new file to do some basic arithmetic

---
+++ fundamentals/src/bin/08_numeric_types.rs
@@ -0,0 +1,15 @@
+fn sub(num1: i32, num2: i32) -> i32 {
+    num1 - num2
+}
+
+
+fn main() {
+    let x = sub(4,2);
+    let sum = 2 + 2;
+    let value = 10 - 5;
+    let division = 10 / 2;
+    let mult = 5 * 5;
+    let rem = 6 % 3;
+    let rem2 = 7 % 3;
+    println!("{x} {sum} {value} {division} {mult} {rem} {rem2}");
+}
