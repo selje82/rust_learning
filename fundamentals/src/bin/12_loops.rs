2ba8a5d0a84c8edbd0a70b049277587e6726f739
Glenn Seljeseth <selje82@hotmail.com>
2026-09-06 19:54:26 +0200

    Added a while loop

--- fundamentals/src/bin/12_loops.rs
+++ fundamentals/src/bin/12_loops.rs
@@ -8,4 +8,12 @@ fn main() {
         }
     }
     println!("Done!");
+    println!("");
+    
+    let mut my_num = 5;
+    while my_num > 0 {
+        println!("{my_num}");
+        my_num -= 1;
+    }
+    println!("Done");
 }
